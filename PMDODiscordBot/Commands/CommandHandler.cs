8a3dddc1cb2361bc0f91d88d75b5b95942ff57f6
John Strebeck <thegreatplanetmojang@gmail.com>
2017-08-24 14:20:10 -0500

    Revert "Sorted commands and added boo u"
    This reverts commit ee0027ab65cac85d82c3a99aa34e9497e61e5cd0.

--- PMDODiscordBot/Commands/CommandHandler.cs
+++ PMDODiscordBot/Commands/CommandHandler.cs
@@ -360,25 +360,6 @@ namespace CSharpDewott.Commands
             {
                 await message.Channel.SendMessageAsync("no u");
             }
-
-            if (message.Content.ToLower().Contains("boo") && message.Content.ToLower().Contains("u") && message.Content.Contains(client.CurrentUser.Mention))
-            {
-                string booU = "boo";
-
-                for (int i = 0; i < Globals.Random.Next(7); i++)
-                {
-                    booU += "o";
-                }
-
-                booU += " ";
-
-                for (int i = 0; i < Globals.Random.Next(7); i++)
-                {
-                    booU += "u";
-                }
-
-                await message.Channel.SendMessageAsync($"no {booU} {message.Author.Mention}");
-            }
         }
     }
 }
